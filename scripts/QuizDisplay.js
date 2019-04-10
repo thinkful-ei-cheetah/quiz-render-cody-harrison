d3648dba05f22033cb31e99c0bd56296ce8bb95b
Cody Gillette <cgillette1312@gmail.com>
2019-04-10 09:01:14 -0700

    more updates

--- scripts/QuizDisplay.js
+++ scripts/QuizDisplay.js
@@ -3,10 +3,10 @@
 
 class QuizDisplay extends Renderer {    // eslint-disable-line no-unused-vars
   getEvents() {
-    this.model.startNewGame();
     return {
       'click .start': 'handleStart',
-      'click .submit': 'nextQuestion'
+      'click .submit': 'handleAnswerQuestion',
+      'click .continue': 'handleNextQuestion'
     };
   }
 
@@ -16,6 +16,9 @@ class QuizDisplay extends Renderer {    // eslint-disable-line no-unused-vars
         <p>
           Welcome to the Trivia Quiz
         </p>
+        <p>
+          Test your Smarts and see how high you can score!
+        </p>
       </div>
       <div>
         <button class="start">Start</button>
@@ -24,29 +27,95 @@ class QuizDisplay extends Renderer {    // eslint-disable-line no-unused-vars
   }
 
   _generateQuestion() {
-    let answerchoices=``; 
+    let answerchoices=''; 
     this.model.unasked[0].answers.forEach(answer=>answerchoices+=`<li><input name="answer" class="answer" type="radio"  value="${answer}"><label> ${answer}</lable></li>`); 
     return `<form>${this.model.unasked[0].text} 
     ${answerchoices}<button class="submit" type="button">Submit</button></form>`;
   }
+  _generateFeedback(){
+    const current = this.model.currentQuestion();
+    let html = '';
+    if(current.answerStatus() === 1){
+      html += `
+      <div>
+      <p>
+        Great job! You got the right answer.
+      </p>
+      <p>
+        ${current.correctAnswer}
+      </p>
+      </div>
+      `;
+    }else{
+      html+= `
+      <div>
+      <p>
+       You answered : ${current.userAnser}
+      </p>
+      <p>
+        The correct answer is :
+      <p>
+        ${current.correctAnswer}
+      </p>
+      </div>
+      `;
+    }
+    html += `
+    <div>
+      <button class="continue">Continue</button>
+    </div>`;
+    return html;
+  }
+
+  _generateOutro(){
+    let highText = '';
+    if(this.model.newhighScore()){
+      highText =`
+      <p>
+        You have a new high score!
+      </p>`;
+    }
+    return `
+    <div>
+      <p>
+      Thanks for Playing!
+      </p>
+      ${highText}
+      <div>
+        <button class="start-quiz">Play Again</button>
+      </div>
+      </div>`;
+  }
 
 
   template() {
-    if (this.model.active) {
-      return this._generateQuestion();
-    } else {
-      return this._generateIntro();
+    let html;
+    const currentQuestion = this.model.correctQuestion();
+    const answerStatus = currentQuestion && currentQuestion.answerStatus();
+
+    if (this.model.asked.length === 0) {
+      html = this._generateIntro();
+    } else if(this.model.active && answerStatus === -1) {
+      html = this._generateQuestion();
+    } else if(this.model.active && answerStatus !== -1){
+      html = this._generateFeedback();
+    }else{
+      html = this._generateOutro();
     }
+    return html;
   }
 
   handleStart() {
-    
-    this.model.toggleActive();
-    this.model.update();
+    this.model.startNewGame();
   }
-  nextQuestion(){
+
+  handelAnswerQuestion(e){
+    e.preventDefault();
+    const userAnswer = e.target.answer.value;
+    this.model.answerQuestion(userAnswer);
+  }
+  handleNextQuestion(){
     this.model.nextQuestion();
-    this.model.update();
     
   }
 }
